18685f045fa8e024dd2c2f9c990d5c8342ba7a7b
angriseno-cyber <angriseno@gmail.com>
2026-02-09 17:54:14 +0700

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -2,10 +2,10 @@ export async function onRequest(context) {
   const { request } = context;
   const url = new URL(request.url);
 
-  // mode ?url=
+  // mode query ?url=
   let target = url.searchParams.get("url");
 
-  // fallback: path-based
+  // fallback path-based
   if (!target) {
     const path = url.pathname.slice(1);
     if (path.startsWith("http://") || path.startsWith("https://")) {
@@ -17,19 +17,39 @@ export async function onRequest(context) {
     return new Response("Missing target url", { status: 400 });
   }
 
+  let targetUrl;
   try {
-    const resp = await fetch(target, {
+    targetUrl = new URL(target);
+  } catch {
+    return new Response("Invalid target url", { status: 400 });
+  }
+
+  try {
+    const resp = await fetch(targetUrl.toString(), {
       headers: {
-        "User-Agent": request.headers.get("user-agent") || "Mozilla/5.0",
-        "Referer": new URL(target).origin
+        "accept": "*/*",
+        "accept-encoding": "identity;q=1, *;q=0",
+        "accept-language": "en-US,en;q=0.9",
+        "priority": "i",
+        "range": "bytes=0-",
+        "referer": "https://cloud.hownetwork.xyz/",
+        "sec-ch-ua": "\"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"144\", \"Google Chrome\";v=\"144\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "video",
+        "sec-fetch-mode": "no-cors",
+        "sec-fetch-site": "cross-site",
+        "sec-fetch-storage-access": "active",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
       }
     });
 
+    // forward response apa adanya
     return new Response(resp.body, {
       status: resp.status,
       headers: resp.headers
     });
-  } catch (e) {
+  } catch (err) {
     return new Response("Fetch error", { status: 502 });
   }
 }
